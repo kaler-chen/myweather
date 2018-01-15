5bac03e6e2e407a03e105f082b6fe884c595db09
kaler-chen <kaler_chen@163.com>
2018-01-15 23:38:03 +0800

    创建常量工具类

---
+++ app/src/main/java/com/myweather/android/util/ConstUtil.java
@@ -0,0 +1,24 @@
+package com.myweather.android.util;
+
+/**
+ * TODO
+ * Created by kaler-chen on 2018/1/15.
+ * com.myweather.android.util
+ */
+
+public class ConstUtil {
+
+    public static final String NOW_WEATHER = "now_weather";
+    public static final String DAILY_WEATHER = "daily_weather";
+    public static final String LIFE_INDEX = "life_index";
+
+    public static final String LOCATION_NAME = "location_name";
+
+    public static final String NOW_WEATHER_URL = "nowWeatherUrl";
+    public static final String DAILY_WEATHER_URL = "dailyWeatherUrl";
+    public static final String LIFE_INDEX_URL = "lifeIndexUrl";
+
+    public static final String BING_PIC = "bing_pic";
+    public static final String BING_PIC_URL = "bingPicUrl";
+    public static final String BING_URL = "bingUrl";
+}
